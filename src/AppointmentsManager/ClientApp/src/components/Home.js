38de16b45c2e2a108112dad246ae47b8ea5590aa
Leo Campos <leopcampos85@gmail.com>
2023-09-24 19:27:12 -0300

    Terminando o componente New.Js

--- src/AppointmentsManager/ClientApp/src/components/Home.js
+++ src/AppointmentsManager/ClientApp/src/components/Home.js
@@ -1,79 +1,146 @@
+import { useEffect, useState } from "react"
 import { Delete } from "./Delete"
 import { Edit } from "./Edit"
 import { New } from "./New"
-import "../custom.css";
 import Appointment from "./Appointment";
-import { testData } from "./Lib"
-import { useEffect } from "react";
-import { useState } from "react";
+import { getDefault, openModal, filter, getAppointments, notifyUser } from "./Lib"
+import "../custom.css";
 
 export default function Home(props) {
 
-  const [dataList, setDataList] = useState([]);
+  const [dataList, setDataList] = useState([])
+
+  const [refreshData, setRefreshData] = useState(0)
+  const [stateListener, setStateListener] = useState(0)
+
+  const filterApp = (e) => {
+    let name_ = e.target.name;
+    let v_ = e.target.value;
+
+    if (name_ === "All" || name_ === "Done" || name_ === "Deleted") {
+      v_ = e.target.checked;
+      filter[name_] = v_;
+    }
+
+    if (name_ === "period") {
+      // 1 = today, 2 = this week, 3 = last week
+      let sd_ = new Date(), ed_ = new Date();
+      const dayNum = sd_.getDay();
+
+      if (v_ === "1") {
+        sd_.setDate(dayNum - 1)
+      }
+
+      if (v_ === "2") {
+        let startDaysInSec = (dayNum - 1) * 24 * 60 * 60 * 1000;
+        let endDaysInSec = (7 - dayNum) * 24 * 60 * 60 * 1000;
+
+        sd_ = new Date(Date.now() - startDaysInSec);
+        ed_ = new Date(Date.now() + endDaysInSec);
+      }
+
+      if (v_ === "3") {
+        let startDaysInSec = dayNum * 24 * 60 * 60 * 1000;
+        let endDaysInSec = (6 + dayNum) * 24 * 60 * 60 * 1000;
+
+        ed_ = new Date(Date.now() - startDaysInSec);
+        sd_ = new Date(Date.now() - endDaysInSec);
+      }
+
+      filter.StartDate = v_ === '4' ? null : sd_;
+      filter.EndDate = v_ === '4' ? null : ed_;
+      filter.SpecifiedDate = null
+    }
+
+    if (name_ === "SpecifiedDate") {
+      filter.SpecifiedDate = new Date(v_);
+      filter.StartDate = null
+      filter.EndDate = null
+    }
+
+    if (name_ === "SpecifiedTime") {
+      filter.SpecifiedTime = v_;
+    }
+
+    if (name_ === "LevelOfImportance") {
+      filter.LevelOfImportance = Number(v_) === 9 ? null : Number(v_);
+    }
+
+    // fetch data with filter
+    getAppointments(filter).then(r => {
+      if (r.length < 1) {
+        notifyUser("Filter result is empty!")
+      }
+      setDataList(r)
+    }).catch(e => console.log("Error getting data on filter: ", e))
+  }
+
 
   useEffect(() => {
-    setDataList(testData)
-  }, [])
+    getDefault().then(data => {
+      setDataList(data)
+    }).catch(e => console.log("Error inside home: ", e))
+  }, [refreshData])
   return (
     <main>
       <h1>Manage Your Appointments / Dates very easy</h1>
-      <p>This powerful web application helps you to menage your dates very easy.</p>
+      <p>This powerful web applicaiton helps you to manage your dates very easy.</p>
       <div className="add-btn row items-center content-center">
-        <div className="btn add">+</div>
+        <div className="btn add" onClick={() => openModal("new-modal")}>+</div>
       </div>
 
-      <div className="notifications spacer-20">This is a test TEXT</div>
+      <div className="notifications spacer-20"></div>
 
       <section className="row justify-btw items-center filter">
         <div className="modal-title">Filter</div>
         <div className="row items-center filter-items">
-          <button className="me-15">Clear Filters</button>
+          <button className="me-15" onClick={()=> window.location.reload()}>Clear Filters</button>
           <div>
-            <label htmlFor="All_f">All</label><br />
-            <input type="checkbox" name="All_f" id="All" />
+            <label htmlFor="All_f">All</label> <br />
+            <input type="checkbox" id="All_f" name="All" onChange={filterApp} />
           </div>
 
           <div>
-            <lable htmlFor="Done_f">Done</lable><br />
-            <input type="checkbox" name="Done_f" id="Done" />
+            <label htmlFor="Done_f">Done</label> <br />
+            <input type="checkbox" id="Done_f" name="Done" onChange={filterApp} />
           </div>
 
           <div>
-            <lable htmlFor="Deleted_f">Deleted</lable><br />
-            <input type="checkbox" name="Deleted_f" id="Deleted" />
+            <label htmlFor="Deleted_f">Deleted</label> <br />
+            <input type="checkbox" id="Deleted_f" name="Deleted" onChange={filterApp} />
           </div>
           <div>
-            <lable htmlFor="period">Period</lable><br />
-            <select name="period" id="period" defaultValue={"4"}>
+            <label htmlFor="period">Period</label> <br />
+            <select name="period" id="period" defaultValue={"4"} onChange={filterApp}>
               <option value="5" disabled>Period</option>
-              <option value="4" disabled>Default</option>
-              <option value="1" disabled>Today</option>
-              <option value="2" disabled>This Week</option>
-              <option value="3" disabled>Last Week</option>
+              <option value="4" >Default</option>
+              <option value="1">Today</option>
+              <option value="2">This week</option>
+              <option value="3">Last week</option>
             </select>
           </div>
 
-          <dvi>
-            <lable htmlFor="SpecifiedDate">Specified Date</lable><br />
-            <input type="date" name="SpecifiedDate" id="SpecifideDate" />
-          </dvi>
-
-          <dvi>
-            <lable htmlFor="SpecifiedTime">Specified Time</lable><br />
-            <input type="time" name="SpecifiedTime" id="SpecifiedTime" />
-          </dvi>
+          <div>
+            <label htmlFor="SpecifiedDate">Specified Date</label> <br />
+            <input type="date" id="SpecifiedDate" name="SpecifiedDate" onChange={filterApp} />
+          </div>
 
           <div>
-            <label htmlFor="LevelOfImportance_f">Level Of Importance</label><br />
-            <select name="LevelOfImportance" id="LevelOfImportance_f" defaultValue={8}>
+            <label htmlFor="SpecifiedTime">Specified Date</label> <br />
+            <input type="time" id="SpecifiedTime" name="SpecifiedTime" onChange={filterApp} />
+          </div>
+
+          <div>
+            <label htmlFor="LevelOfImportance_f">Level Of Importance</label> <br />
+            <select name="LevelOfImportance" id="LevelOfImportance_f" defaultValue={8} onChange={filterApp}>
               <option value={8} disabled>Level Of Importance</option>
-              <option value={9} disabled>Reset</option>
-              <option value={5} disabled>Very High</option>
-              <option value={4} disabled>High</option>
-              <option value={3} disabled>Medium</option>
-              <option value={2} disabled>Normal</option>
-              <option value={1} disabled>Low</option>
-              <option value={0} disabled>Very Low</option>
+              <option value={9} >Reset</option>
+              <option value={5}>Very High</option>
+              <option value={4}>High</option>
+              <option value={3}>Medium</option>
+              <option value={2}>Normal</option>
+              <option value={1}>Low</option>
+              <option value={0}>Very Low</option>
             </select>
           </div>
         </div>
@@ -86,19 +153,31 @@ export default function Home(props) {
         <div className="column importance">Importance</div>
         <div className="column date">Date</div>
         <div className="column time">Time</div>
-        <div className="column address">Address</div>
+        <div className="column addr">Adress</div>
         <div className="column edit">Edit</div>
         <div className="column delete">Delete</div>
       </div>
 
       {
         dataList.length === 0 ?
-          <div className="row mt-15 witing">Loading<div className="loading">...</div></div> :
-          dataList.map(item => <Appointment item={item} key={item.id} />)
+          <div className="row mt-15 waiting">Loading <div className="loading">...</div></div> :
+          dataList.map(item => <Appointment item={item} key={item.id} stateListener={setStateListener} />)
       }
-      <New />
-      <Edit />
-      <Delete />
+
+
+      <section>
+        <section className="modal new-modal hidden">
+          <New refreshApp={setRefreshData} />
+        </section>
+
+        <section className="modal edit-modal hidden">
+          <Edit stateListener={stateListener} refreshApp={setRefreshData} />
+        </section>
+
+        <section className="modal delete-modal hidden">
+          <Delete stateListener={stateListener} refreshApp={setRefreshData} />
+        </section>
+      </section>
     </main>
   )
 }
